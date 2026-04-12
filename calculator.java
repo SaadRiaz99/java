acc4f78a66c5d9d8a04b304f7f6ed987d46f8d71
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-04-12 23:18:48 +0500

    Add File :Calculator

---
+++ calculator.java
@@ -0,0 +1,22 @@
+import java.util.scanner;
+
+
+
+public class Calculator{
+    public static void main(string[] args){
+        scanner input = new scanner(system.in);
+
+
+        system.out.println("Simple Java Calulator")
+
+
+        System.out.println("Simple Java Calculator");
+        System.out.println("Choose an operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        System.out.print("Enter choice (1-4): ");
+    }
+}
