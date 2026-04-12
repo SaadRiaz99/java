8903ae17f0fa432b6a6690b8f74600f8c9e2f716
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-04-12 23:25:00 +0500

    make Callculator

--- calculator.java
+++ calculator.java
@@ -1,13 +1,9 @@
-import java.util.scanner;
-
-
+import java.util.Scanner;
 
 public class Calculator{
-    public static void main(string[] args){
-        scanner input = new scanner(system.in);
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
 
-
-        system.out.println("Simple Java Calulator")
 
 
         System.out.println("Simple Java Calculator");
@@ -18,5 +14,47 @@ public class Calculator{
         System.out.println("4. Division");
 
         System.out.print("Enter choice (1-4): ");
+
+        int choice = input.nextInt();
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        double result;
+
+        switch(choice) {
+            case 1:
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case 2:
+                result = num1 - num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case 3:
+                result = num1 * num2;
+                System.out.println("Result = " + result);
+                break;
+            
+            case 4:
+                if(num2 != 0){
+                    result = num1 / num2;
+                    System.out.println("Result = " + result);
+
+                } else{
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                  break;
+
+            default:
+                System.out.println("Invalid choice!");
+
+        }
+        input.close();
     }
+
 }
