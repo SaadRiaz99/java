a5a3059847e7f9d715575ac2771002cc19262696
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-04-29 13:52:30 +0500

    Add

--- random.java
+++ random.java
@@ -1,2 +1,11 @@
 import java.util.Random;
 import java.util.Scanner;
+
+
+public class numberguessing{
+    public static void main(string args[]){
+        Random rand = new Random();
+        int number = rand.nextInt(100) + 1 ;
+        
+    }
+}
