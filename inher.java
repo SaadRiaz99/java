620a5baef4a1dcffd5bcf672424c05a16c3e77dd
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-05-04 23:27:00 +0500

    Add File :  Double Inheritance

---
+++ inher.java
@@ -0,0 +1,15 @@
+class car {
+    String brand; 
+    int year;
+
+
+
+    public car(String brand, int year){
+        this.brand = brand;
+        this.year = year;
+
+    }
+}
+    public void start(){
+        system.out.println("Car is Starting......")
+    }
