635ab629f269c31cd9c630cd0e98034df836abd4
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-05-04 23:32:19 +0500

    Add First Child

--- inher.java
+++ inher.java
@@ -13,3 +13,26 @@ class car {
     public void start(){
         system.out.println("Car is Starting......")
     }
+
+
+    public void stop() {
+        System.out.println(brand + " car is stopping...");
+    }
+
+
+    public void display() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Year: " + year);
+    }
+
+
+class Electriccar  extends car{
+    int batteryCapicity;
+
+
+    public Electriccar(String brand, int year, int batteryCapicity){
+        super(brand, year);
+        this.batteryCapicity = batteryCapicity;
+    }
+}
+
